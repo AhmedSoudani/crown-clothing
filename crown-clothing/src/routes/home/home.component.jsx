f6cef3f02eb7cc8e1c11f5a879253ed0efeea0f8
SPARATA <ahmedsoudani09@gmail.com>
2024-06-24 20:56:54 +0100

    starting the routing

---
+++ crown-clothing/src/routes/home/home.component.jsx
@@ -0,0 +1,13 @@
+import CategoriesMenu from '../../components/categories-menu/categories-menu.component';
+
+
+const home = () => {
+
+  
+
+  return (
+    <CategoriesMenu />
+  );
+}
+
+export default home;
